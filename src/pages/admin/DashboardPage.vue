fe0d675a2bcc15c8b6ea6f647b7d8b7606318afc
Kevw94 <pinerokevin@icloud.com>
2022-10-05 22:26:53 +0200

    implement listing of users on the admin Dashboard
    Feat:
        - add user's cells
        - add user's card
        - displays all user's that are registered

--- src/pages/admin/DashboardPage.vue
+++ src/pages/admin/DashboardPage.vue
@@ -1,17 +1,17 @@
 <template>
 	<div>
 		<RessourcesCells />
-
-
+		<UsersCells />
 	</div>
 </template>
 
 <script>
 import { useAdminStore } from "../../stores/admin.store";
 import RessourcesCells from '@/components/Admin/ressources/RessourcesCells.vue';
+import UsersCells from '@/components/Admin/users/UsersCells.vue';
 
 export default {
-	components: { RessourcesCells },
+	components: { RessourcesCells, UsersCells },
     setup() {
         const adminStore = useAdminStore();
 
