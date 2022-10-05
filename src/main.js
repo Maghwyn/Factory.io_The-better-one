d6c19d47069e22f26e5f1a6c43db5c2d2018ff38
Maghwyn <christopher.samson@edu.esiee-it.fr>
2022-10-05 14:02:25 +0200

    Added basic template css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from "pinia";
 import App from './App.vue'
 import route from "./router/index";
+import "@/scss/config.scss"
 
 const app = createApp(App);
 app.use(route());
