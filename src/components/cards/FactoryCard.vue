42ccdbe5387c872bba5ea65ed114bb604505a985
Jules <jules.duarte@edu.esiee-it.fr>
2022-10-06 09:34:40 +0200

    #27, début factory card

---
+++ src/components/cards/FactoryCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="border-red-800 border-4 border-solid w-40 h-60 rounded flex flex-col items-center m-4">
+        <div class=" w-4/5 h-2/5 mt-4 rounded border-red-800 border-4 border-solid">
+
+        </div>
+        <div>
+            <p>Type factory</p>
+            <p>Production</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: 'FactoryCard',
+      props: {
+      }
+    }
+</script>
--- src/pages/main/GamePage.vue
+++ src/pages/main/GamePage.vue
@@ -1,3 +1,13 @@
 <template>
     <p>yo</p>
+    <FactoryCard></FactoryCard>
 </template>
+
+<script>
+    import FactoryCard from '@/components/cards/FactoryCard.vue'
+export default {
+    components:{
+    FactoryCard
+}
+}
+</script>
